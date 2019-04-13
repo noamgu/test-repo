f1ba7284c04a28cd031bc1889809139be3890336
noamgu <39539338+noamgu@users.noreply.github.com>
2019-04-13 13:17:30 +0300

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -2,5 +2,5 @@
 echo "This is an example of a script"
 echo "That is running in a build step"
 echo "This is a param $PASSED"
-NUM=$((FIRSTNUMBER + $SECONDNUMBER))
+NUM=$(($FIRSTNUMBER + $SECONDNUMBER))
 echo "$NUM"
